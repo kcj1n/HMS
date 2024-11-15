24fcc85b3e16e9ea8d9f4dc3f6ee553c2d96411d
kcj1n <106325891+kcj1n@users.noreply.github.com>
2024-11-15 13:32:44 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
-/* eslint-disable no-unused-vars */
 import React    from "react";
 import ReactDOM from "react-dom/client";
 import App      from "./App.jsx";
-//import Apps     from "./Apps.jsx";
 import               "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
